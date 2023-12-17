720842642b645be3be624370cb684d83c179f3d1
Anton Bozhiy <abozhiy@gmail.com>
2023-12-17 21:19:28 +0100

    clean spec dir

--- spec/application_helper.rb
+++ spec/application_helper.rb
@@ -1,15 +1,7 @@
 # frozen_string_literal: false
 
 require 'spec_helper'
-require_relative 'support/route_helpers'
 
 ENV['RACK_ENV'] ||= 'test'
 
 require_relative '../config/environment'
-
-abort('You in prod mode!') if Application.environment == :production
-
-RSpec.configure do |config|
-  # config.include Rack::Tests::Methods
-  config.include RouteHelpers, type: :routes
-end
